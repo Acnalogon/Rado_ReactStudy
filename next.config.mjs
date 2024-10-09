f47b7c1bf89069f2a54c294546ed7f41ae70a208
Rado Vidakovic <33847432+Acnalogon@users.noreply.github.com>
2024-10-09 21:00:05 +0200

    created an static version so i can publish on gitH

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+/** type {import('next').NextConfig} */
+/* const nextConfig = {};
+
+export default nextConfig;
+ */
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Tells Next.js to export the app as static files
+  distDir: "out", // Directory where exported files will be stored
+};
 
 export default nextConfig;
