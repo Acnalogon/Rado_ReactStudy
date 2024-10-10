f1d54568ce6e608b972b903b793dcc956f6ecc3f
Rado Vidakovic <33847432+Acnalogon@users.noreply.github.com>
2024-10-10 17:06:00 +0200

    Fix static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,13 @@ export default nextConfig;
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  distDir: "out", // Where your static files will be exported
-  assetPrefix: process.env.NODE_ENV === "production" ? "/Rado_ReactStudy/" : "", // Set asset prefix for production
+  output: "export", // Ensure full static export
+  distDir: "out", // Export directory
+  assetPrefix: process.env.NODE_ENV === "production" ? "/Rado_ReactStudy/" : "", // GitHub Pages specific asset prefix
   images: {
-    unoptimized: true, // Disable Next.js image optimization since GitHub Pages does not support it
+    unoptimized: true, // Disable image optimization
   },
+  trailingSlash: true,
 };
 
 export default nextConfig;
